0b6c9b147b0fa2611a8c75d5c3e21f8fe4531df4
m0mt <ycross4008@gmail.com>
2020-12-20 21:00:47 +0900

    리액트 onClick 이벤트 생성, preventDafault함수 추가

--- src/App.js
+++ src/App.js
@@ -1,36 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TOC from "./components/TOC"
+// import Subject from "./components/Subject"
+import Content from "./components/Content"
 import './App.css';
 
 // function App() {
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
 //   );
 // }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject:{title:'WEB', sub:'World Wide WEb!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'}, 
+      contents: [
+        {id: 1, title:'HTML', desc: 'HTML is for information'},
+        {id: 2, title:'css', desc: 'CSS is for design'},
+        {id: 3, title:'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
+    console.log('App render');
+    let _title, _desc = null;
+    if(this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        {/* <Subject title="React" sub="For UI"></Subject> */}
+        <header>
+            <h1><a href="/" onClick={(e) => {
+              // alert('hi');
+              console.log(e);
+              e.preventDefault();
+            }}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
 
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
