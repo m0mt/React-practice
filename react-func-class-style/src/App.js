b0b32df4ab83b54c7828976fa0ef4fbdab137f88
m0mt <ycross4008@gmail.com>
2020-12-24 20:51:28 +0900

    함수형 컴포넌트에서 componentDidMount 의 사용법

--- react-func-class-style/src/App.js
+++ react-func-class-style/src/App.js
@@ -2,11 +2,19 @@ import React , { Component, useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  let [funcShow, setFuncShow] = useState(true);
+  let [classShow, setclassShow] = useState(true);
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <input type="button" value="remove func" onClick={function() {
+        setFuncShow(false);
+      }}></input>
+      <input type="button" value="remove comp" onClick={function() {
+        setclassShow(false);
+      }}></input>
+      {funcShow ? <FuncComp initNumber={2}></FuncComp> : null }
+      {classShow ? <ClassComp initNumber={2}></ClassComp> : null }
     </div>
   );
 }
@@ -24,18 +32,40 @@ function FuncComp(props) {
   // let setDate = dateState[1];
   // 아래 코드로 변환 가능
   let [_date, setDate] = useState(new Date().toString());
-  
-  useEffect(function() {
-    // componentDidMount, componentDidUpdate 와 똑같은 역할
-    console.log('%cfunc => useEffect (componentDidMount & ComponentDidUpdate) A' +(funcId++), funcStyle);
-    document.title = number + ' : ' + _date;
-  });
 
   useEffect(function() {
+    console.log('%cfunc => useEffect number (componentDidMount) ' +(funcId++), funcStyle);
+    return function() {
+      console.log('%cfunc => useEffect return (componentDidMount) ' +(funcId++), funcStyle);
+    }
+  }, []); // 두번째 인자값에 빈 인자값이면 컴포넌트가 생성될 때 한번만 실행한다 (componentDidMount의 역할)
+  
+  // 변화한 값만 render() 시키기 때문에 손쉽게 성능향상 가능
+  // side effect
+  useEffect(function() {
     // componentDidMount, componentDidUpdate 와 똑같은 역할
-    console.log('%cfunc => useEffect (componentDidMount & ComponentDidUpdate) B' +(funcId++), funcStyle);
-    document.title = number + ' : ' + _date;
-  });
+    console.log('%cfunc => useEffect number (componentDidMount & ComponentDidUpdate) A' +(funcId++), funcStyle);
+    document.title = number;
+    // return 함수를 실행하면 cleanup , useEffect를 다시 실행함.??
+    return function() {
+      console.log('%cfunc => useEffect return (componentDidMount & ComponentDidUpdate) ' +(funcId++), funcStyle);
+    }
+  }, [number]);
+  useEffect(function() {
+    // componentDidMount, componentDidUpdate 와 똑같은 역할
+    console.log('%cfunc => useEffect date (componentDidMount & ComponentDidUpdate) A' +(funcId++), funcStyle);
+    document.title = _date;
+    // return 함수를 실행하면 cleanup , useEffect를 다시 실행함.??
+    return function() {
+      console.log('%cfunc => useEffect return (componentDidMount & ComponentDidUpdate) ' +(funcId++), funcStyle);
+    }
+  }, [_date]);
+
+  // useEffect(function() {
+  //   // componentDidMount, componentDidUpdate 와 똑같은 역할
+  //   console.log('%cfunc => useEffect (componentDidMount & ComponentDidUpdate) B' +(funcId++), funcStyle);
+  //   document.title = number + ' : ' + _date;
+  // });
   
   console.log('%cfunc => render ' + (funcId++), funcStyle);
   return (
@@ -77,6 +107,9 @@ class ClassComp extends Component {
   }
   componentDidUpdate() {
     console.log('%cclasss => componentDidUpdate', classStyle);
+  }
+  componentWillUnmount() {
+    console.log('%cclasss => componentWillUnmount', classStyle);
   }
   render() {
     console.log('%cclass => render', classStyle);
