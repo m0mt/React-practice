3e3eeb15648a49d745f50b2d81b6aa1ed38d6ede
m0mt <ycross4008@gmail.com>
2021-01-01 20:42:33 +0900

    redux를 도입했을때 발생되는 효과

--- react-redux/src/src/components/AddNumber.js
+++ react-redux/src/src/components/AddNumber.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import store from '../store';
+
 
 export default class AddNumber extends Component {
     state = {size: 1}
@@ -7,9 +9,7 @@ export default class AddNumber extends Component {
             <div>
                 <h1>Add Number</h1>
                 <input type="button" value="+" onClick={function() {
-                    // 컴포넌트가 무수히 많아질수록 onClick 처럼 안에 있는 함수를 
-                    // 무수히 많이 작성해야됨.
-                    this.props.onClick(this.state.size);
+                    store.dispatch({type:'INCREMENT', size: this.state.size});
                 }.bind(this)}></input>
                 <input type="text" value={this.state.size} onChange={function(e) {
                     this.setState({size: Number(e.target.value)})
@@ -18,3 +18,23 @@ export default class AddNumber extends Component {
         )
     }
 }
+
+// redux를 사용 하지 않았을 때
+// export default class AddNumber extends Component {
+//     state = {size: 1}
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Add Number</h1>
+//                 <input type="button" value="+" onClick={function() {
+//                     // 컴포넌트가 무수히 많아질수록 onClick 처럼 안에 있는 함수를 
+//                     // 무수히 많이 작성해야됨.
+//                     this.props.onClick(this.state.size);
+//                 }.bind(this)}></input>
+//                 <input type="text" value={this.state.size} onChange={function(e) {
+//                     this.setState({size: Number(e.target.value)})
+//                 }.bind(this)}></input>
+//             </div>
+//         )
+//     }
+// }
