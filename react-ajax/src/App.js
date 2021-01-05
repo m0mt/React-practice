23211735ff1dfbc37fbb0442d303f1ee53da62b2
m0mt <ycross4008@gmail.com>
2021-01-05 19:10:16 +0900

    Ajax 통신을 통해 컴포넌트 초기화, 상태 변경하기

--- react-ajax/src/App.js
+++ react-ajax/src/App.js
@@ -1,22 +1,77 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
+class Nav extends Component {
+  state = {
+    list: []
+  }
+  componentDidMount() {
+    fetch('list.json')
+      .then(function(result) {
+        return result.json();
+      }) // 가져온 데이터를 어떤 데이터 타입으로 제어 할것인지를 첫번째 then에 적어줌.
+      .then(function(json){ // 
+        this.setState({list:json});
+      }.bind(this));
+  }
+  render() {
+    let listTag = [];
+    for (let data of this.state.list) {
+      listTag.push(
+        <li key={data.id}>
+          <a href={data.id} data-id={data.id} onClick={function(e){
+            e.preventDefault();
+            this.props.onClick(e.target.dataset.id);
+          }.bind(this)}>{data.title}</a>
+        </li>
+      )
+    }
+    return (
       <nav>
         <ul>
-          <li><a href = "1">HTML</a></li>
-          <li><a href = "2">CSS</a></li>
-          <li><a href = "3">Javascript</a></li>
+          {listTag}
         </ul>
       </nav>
+    );
+  }
+}
+class Article extends Component {
+  render() {
+    return (
       <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    article: {title:'Welcome', desc:'Hello, React & Ajax'}
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        <Nav onClick={function(id){
+          fetch(id + '.json')
+            .then(function(result) {
+              return result.json();
+            })
+            .then(function(json) {
+              console.log(json);
+              this.setState({  
+                article: {
+                  title: json.title,
+                  desc: json.desc
+                }
+              })
+            }.bind(this));
+        }.bind(this)}></Nav>
+        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
