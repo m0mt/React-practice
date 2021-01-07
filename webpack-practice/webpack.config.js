5c46a0a60915b19ddb80676e95d6ea4a6d0cbee6
m0mt <ycross4008@gmail.com>
2021-01-07 17:59:40 +0900

    webpack css loader의 도입

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -7,5 +7,18 @@ module.exports = {
     },
     // mode : 'development' || 'production' || 'none'
     // description: webpack.js.org/configuration
-    mode: 'development'
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    // 뒤쪽에 있는 css-loader가 먼저 실행 되므로
+                    // style-loader를 먼저 실행 했을 시 오류발생
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 }
