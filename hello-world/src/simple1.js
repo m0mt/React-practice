19e56a6d6385f463c265c47e5d736f5d919009b8
m0mt <ycross4008@gmail.com>
2021-01-12 16:42:34 +0900

    React 프로그래밍 3강 'Babel 사용해 보기'

---
+++ hello-world/src/simple1.js
@@ -0,0 +1,51 @@
+function LikeButton() {
+    const [liked, setLiked] = React.useState(false);
+    const text = liked ? '좋아요 취소' : '좋아요';
+    // createElement 를 jsx 문법으로 변환 
+    return <button onClick={() => setLiked(!liked)}>{text}</button>;
+    // return React.createElement(
+    //     'button', 
+    //     { onClick: () => setLiked(!liked) },
+    //     text,
+    // );
+}
+
+function Container() {
+    const [count, setCount] = React.useState(0);
+    // createElement 를 jsx 문법으로 변환 
+    return (
+        <div>
+            <LikeButton />
+            <div style={{ marginTop: 20 }}>
+                <span>현재 카운트: </span>
+                <span style={{ marginRight: 20 }}>{count}</span>    
+                <button onClick={() => setCount(count + 1)}>증가</button>
+                <button onClick={() => setCount(count - 1)}>감소</button>
+            </div>
+        </div>
+    );
+    // return React.createElement(
+    //     'div',
+    //     null,
+    //     React.createElement(LikeButton),
+    //     React.createElement(
+    //         'div',
+    //         { style: { marginTop: 20 } },
+    //         React.createElement('span', null, '현재 카운트: '),
+    //         React.createElement('span', { style: { marginRight: 10 } }, count),
+    //         React.createElement(
+    //             'button',
+    //             { onClick: () => setCount(count + 1) },
+    //             '증가',
+    //         ),
+    //         React.createElement(
+    //             'button',
+    //             { onClick: () => setCount(count - 1) },
+    //             '감소',
+    //         ),
+    //     ),
+    // );
+}
+
+const domContainer = document.getElementById('root');
+ReactDOM.render(React.createElement(Container), domContainer);
