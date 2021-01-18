98801cc9d1d54937bcb492229bbf480069666cf9
m0mt <ycross4008@gmail.com>
2021-01-18 20:34:17 +0900

    React 프로그래밍 10강 '컴포넌트의 속성값과 상태값'

--- react-inflearn/3장/live/src/App.js
+++ react-inflearn/3장/live/src/App.js
@@ -1,48 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from './Counter';
 
-function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [currentId, setCurrentId] = useState(1);
-  const [desc, setDesc] = useState('');
-  const [showOdd, setShowOdd] = useState(false);
-
-  function onAdd() {
-    const todo = { id: currentId, desc };
-    setCurrentId(currentId + 1);
-    setTodoList([...todoList, todo]);
-  }
-
-  function onDelete(e) {
-    const id = Number(e.target.dataset.id);
-    const newTodoList = todoList.filter(todo => todo.id !== id);
-    setTodoList(newTodoList);
-  }
-
-  function onsaveToServer() {}
-
+export default function App() {
+  // const [color, setColor] = useState('red');
+  // function onClick() {
+  //   setColor('blue');
+  // }
   return (
-    <div className="App">
-      <h3>할 일 목록</h3>
-      <ul>
-        {todoList
-          .filter((_, index) => (showOdd ? index % 2 === 0 : true))
-          .map(todo => (
-          <li key={todo.id}>
-            <span>{todo.desc}</span>
-            <button data-id={todo.id} onClick={onDelete}>
-              삭제
-            </button>
-          </li>
-        ))}
-      </ul>
-      <input type="text" value={desc} onChange={e => {setDesc(e.target.value)}} />
-      <button onClick={onAdd}>추가</button>
-      <button onClick={() => setShowOdd(!showOdd)}>
-        홀수 아이템만 보기 on/off
-      </button>
-      <button onClick={onsaveToServer}>서버에 저장</button>
+    <div>
+      <Counter />
+      {/* <button style={{ backgroundColor: color }} onClick={onClick}>
+        좋아요
+      </button> */}
     </div>
   );
 }
-
-export default App;
