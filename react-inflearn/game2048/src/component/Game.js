31cbc52d5ab79fa986825aaf07155776fd9c1cf4
m0mt <ycross4008@gmail.com>
2021-01-23 19:20:23 +0900

    React 프로그래밍 game2048 '키보드 동작과 블록 merge이벤트'

--- react-inflearn/game2048/src/component/Game.js
+++ react-inflearn/game2048/src/component/Game.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 import times from 'lodash/times';
 import { MAX_POS } from '../constant';
 import { getInitialTileList } from '../util/tile';
+import useMoveTile from '../hook/useMoveTile';
+import Tile from './Tile';
 
-export default function Game() {
+export default function Game({ setScore }) {
   const [tileList, setTileList] = useState(getInitialTileList);
+  // up, down, left, right
+  useMoveTile({tileList, setTileList, setScore });
   return (
     <div className="game-container">
       <div className="grid-container">
-        {times(MAX_POS, () => (
-          <div className="grid-row">
-            {times(MAX_POS, () => (
-              <div className="grid-cell"></div>
+        {times(MAX_POS, index => (
+          <div key={index} className="grid-row">
+            {times(MAX_POS, index2 => (
+              <div key={index2} className="grid-cell"></div>
             ))}
           </div>
         ))}
@@ -19,9 +23,7 @@ export default function Game() {
 
       <div className="tile-container">
         {tileList.map(item => (
-          <div className={`tile tile-${item.value} tile-position-${item.x}-${item.y}`}>
-            <div className="tile-inner">{item.value}</div>
-          </div>
+          <Tile key={item.id} {...item} />
         ))}
       </div>
     </div>
