ae6eb36dfb1c8068cc92fc0b505829bfb6fd1b98
m0mt <ycross4008@gmail.com>
2021-02-22 23:50:22 +0900

    React 프로그래밍 22강 '속성값 타입 정의하기: prop-types'

---
+++ react-inflearn/live/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import User from './User';
+
+export default function App() {
+  return (
+    // <div>
+      // {/* <p>실전 리액트</p> */}
+      <User onChangeTitle={() => {}} male={123}></User>
+    // </div>
+  );
+}
