841c708ca6ce9c72ee84419f3eb0f782aa39e7ee
m0mt <ycross4008@gmail.com>
2021-03-24 18:42:03 +0900

    React 프로그래밍 37강 'redux-saga를 이용한 비동기 액션 처리 1'

--- react-inflearn/live/src/common/store.js
+++ react-inflearn/live/src/common/store.js
@@ -1,11 +1,26 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import timelineReducer from '../timeline/state';
+import timelineSaga from '../timeline/state/saga';
 import friendReducer from '../friend/state/index';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
 
 const reducer = combineReducers({
     timeline: timelineReducer,
     friend: friendReducer,
 });
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
+
+function* rootSaga() {
+    yield all([timelineSaga()]);
+}
+sagaMiddleware.run(rootSaga);
+
 export default store;
